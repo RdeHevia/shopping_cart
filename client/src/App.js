c92c0a9420ffc21ae8936fc8c9a7b377660ba0ab
Stephanie <stephanie.cunnane@gmail.com>
2021-09-28 12:11:02 -0700

    Fetch data using Axios; send POSTs to add new product

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 
+import axios from "axios";
+
 import Header from "./components/Header.js";
 import Products from "./components/Products.js";
 import AddForm from "./components/AddForm.js";
-import axios from "axios";
-import data from "./lib/data.js";
 
 /*
 X Fetch product data 
 X Add Product button: onClick -> show the add product form (conditional product) {!isHidden && <Button />}
-- hide add product form
+- hide add product form 
+      - happens after ADDING a product
+      X also happens after hitting CANCEL
 - Add product form -> send a POST request to backend API
 - hide/show edit product form
 
@@ -36,21 +38,40 @@ const App = () => {
     setIsAddProductFormHidden(false);
   };
 
+  const handleHideAddProductForm = (event) => {
+    event.preventDefault();
+    setIsAddProductFormHidden(true);
+  };
+
+  const handleFormSubmission = async (newProduct, callback) => {
+    try {
+      const response = await axios.post("/api/products", {
+        title: productName,
+        price,
+        quantity,
+      });
+      const newProduct = response.data;
+      setProducts([...products, newProduct]);
+
+      if (callback) {
+        callback();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div id="app">
       <Header />
 
       <main>
         <Products products={products} />
-        {/* <a
-          className="button add-product-button"
-          onClick={handleShowAddProductForm}
-        >
-          Add A Product
-        </a> */}
         <AddForm
+          onCancelClick={handleHideAddProductForm}
           isHidden={isAddProductFormHidden}
           onShowAddProduct={handleShowAddProductForm}
+          onSubmit={handleFormSubmission}
         />
       </main>
     </div>
--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,12 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
 import AddProductButton from "./AddProductButton";
 
-const AddForm = ({ isHidden, onShowAddProduct }) => {
-  // {isAddFormHidden ? "add-form" : "add-form visible"}
+/*
+  X define state: productName, price, quantity
+  X handlers to control inputs: onChange={handleBlabla}
+  - on Add buttton: onSubmit={handleAddNewProduct}
+         handler defined in this component
+         get at all the input values
+         use those input values as the json for a POST request
+
+  - (FUTURE) input validation
+  - (FUTURE) recycle the component (same form for add and edit product)
+*/
+
+/*
+  {
+  "title": "string",
+  "price": 0,
+  "quantity": 0
+}
+*/
+
+const AddForm = ({ isHidden, onShowAddProduct, onCancelClick, onSubmit }) => {
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  const handleAddProduct = async () => {
+    const response = await axios.post("/api/products", {
+      title: productName,
+      price,
+      quantity,
+    });
+
+    const newProduct = response.data;
+    // newProduct = response.data
+    // add the new product to the products array (const [products, setProducts] = useState([]);)
+  };
+  // WE ARE HERE!!!!!!!!
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   onSubmit({ author, body }, resetInputs);
+  // };
+
+  // const resetInputs = () => {
+  //   setAuthor("");
+  //   setBody("");
+  // };
+
   return (
     <div className={isHidden ? "add-form" : "add-form visible"}>
-      {/* <p>
-        <a className="button add-product-button">Add A Product</a>
-      </p> */}
       <p>
         <AddProductButton onClick={onShowAddProduct} />
       </p>
@@ -14,22 +58,41 @@ const AddForm = ({ isHidden, onShowAddProduct }) => {
       <form>
         <div className="input-group">
           <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value="" />
+          <input
+            type="text"
+            id="product-name"
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+          />
         </div>
 
         <div className="input-group">
           <label for="product-price">Price</label>
-          <input type="text" id="product-price" value="" />
+          <input
+            type="text"
+            id="product-price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
         </div>
 
         <div className="input-group">
           <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value="" />
+          <input
+            type="text"
+            id="product-quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
         </div>
 
         <div className="actions form-actions">
-          <a className="button">Add</a>
-          <a className="button">Cancel</a>
+          <a className="button" onClick={handleAddProduct}>
+            Add
+          </a>
+          <a className="button" onClick={onCancelClick}>
+            Cancel
+          </a>
         </div>
       </form>
     </div>
